47b0044f4094be2941fc485f05cfe0490452936f
Eldon Hopp <elhf8@umsystem.edu>
2025-02-23 18:33:35 -0600

    Added isIdentifier, isOperator, IsInteger functions and made a few changes to the over all code.
    Moving on to Debugging

---
+++ cmake-build-debug/CMakeFiles/4450_Project_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "4450_Project_1.exe"
+  "4450_Project_1.exe.manifest"
+  "4450_Project_1.pdb"
+  "CMakeFiles/4450_Project_1.dir/main.cpp.obj"
+  "CMakeFiles/4450_Project_1.dir/main.cpp.obj.d"
+  "lib4450_Project_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4450_Project_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/4450_Project_1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 4450_Project_1.
+# This may be replaced when dependencies are built.
